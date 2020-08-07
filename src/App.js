8f77a35c829f36f7c14397a598ac445c88f7199f
wolfdog27 <kylenicholasanderson@gmail.com>
2020-08-06 21:27:27 -0700

    changed to class App, with state, add empContainer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React Employee Directory</h1>
-      <label htmlFor="numInput">
-        # of Emps
-        <input id="numInput" name="numInput" />
-      </label>
-      <button>Submit</button>
-    </div>
-  );
+class App extends Component {
+  state = {
+    numInput: 0,
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>React Employee Directory</h1>
+        <label htmlFor="numInput">
+          # of Emps
+        <input
+            id="numInput"
+            name="numInput"
+            value={this.state.numInput}
+            type="number"
+            min="0" />
+        </label>
+        <button>Submit</button>
+
+        <div className="empContainer">
+          {this.state.numInput}
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
